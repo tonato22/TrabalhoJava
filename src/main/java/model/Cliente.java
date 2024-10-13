ba81eaff22a89236903303b4a3555b407e83c4b4
João Victor <151487317+joaovictords11@users.noreply.github.com>
2024-10-13 19:38:45 -0300

    atualizando Cliente

---
+++ src/main/java/model/Cliente.java
@@ -0,0 +1,28 @@
+// src/main/java/model/Cliente.java
+package model;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String endereco;
+
+    public Cliente(String nome, String cpf, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    // Adicione setters se necessário
+}
